0ba3a338c21b0a5ee8df1fe4cfcf9859d7ec4465
91917 <91917@host.docker.internal>
2022-01-13 14:54:58 +0530

    Creating Student class

--- JavaBasics/src/Basics/HelloWorld.java
+++ JavaBasics/src/Basics/HelloWorld.java
@@ -3,5 +3,8 @@ package Basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		
+		Student s = new Student();
+		System.out.println(s.score);
 	}
 }
---
+++ JavaBasics/src/Basics/Student.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class Student {
+	int score;
+	
+	Student(){
+		score = 0;
+	}
+
+}
